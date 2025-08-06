a54d00a8c6fdfdc8a48d907bed4cc0593bbf1a80
Jhonny Lima <lima.jhonny95@gmail.com>
2025-08-05 23:00:07 -0300

    refactor: update `from_small_unit` function to return a float value

--- utils/money_utils.py
+++ utils/money_utils.py
@@ -3,5 +3,5 @@ from decimal import Decimal, ROUND_HALF_UP
 def to_small_unit(amount: Decimal) -> int:
   return int((amount * 100).to_integral_value(rounding=ROUND_HALF_UP))
 
-def from_small_unit(amount: int) -> Decimal:
-  return Decimal(amount) / 100
+def from_small_unit(amount: int) -> float:
+  return float(Decimal(amount).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP) / 100)
