be7e7e753e88114a08a58739b9b7acb3c0279fbd
Jhonny Lima <lima.jhonny95@gmail.com>
2025-08-05 23:01:32 -0300

    feat: implement the create payment route

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from repository.database import db
 from models.payment import Payment
+from datetime import datetime, timedelta
+from utils.money_utils import to_small_unit, from_small_unit
+from decimal import Decimal
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -9,13 +12,23 @@ db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
-  # Here you would implement the logic to create a PIX payment
-  # For now, we will return a mock response
-  response = {
-    "status": "success",
-    "message": "PIX payment created successfully"
-  }
-  return jsonify(response), 201
+  data = request.get_json()
+  if not data or 'amount' not in data:
+    return jsonify({"error": "Invalid data"}), 400
+  try:
+    amount = to_small_unit(Decimal(data['amount']))
+    expiration_date = datetime.now() + timedelta(minutes=30)
+    new_payment = Payment(amount=amount, expiration_date=expiration_date)
+    db.session.add(new_payment)
+    db.session.commit()
+  except Exception as e:
+    return jsonify({"error": str(e)}), 500
+  else:
+    return jsonify({
+      "status": "success",
+      "message": "PIX payment created successfully",
+      "payment": new_payment.to_dict(),
+    }), 201
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def confirm_payment_pix():
